c22d6997afec67f035d7807ab03be331e126401b
Suryansh Sugara <73980109+suryanshsugara@users.noreply.github.com>
2025-04-06 15:42:36 +0530

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,11 @@
+#include <WiFi.h>
+#include "ThingSpeak.h"
+#include <DHT.h>
+
+// WiFi credentials
+#define SECRET_SSID "YOUR WIFI NAME"
+#define SECRET_PASS "YOUR WIFI PASSWORD"
+
+// ThingSpeak credentials
+#define SECRET_CH_ID 2823262
+#define SECRET_WRITE_APIKEY "48DDC76
